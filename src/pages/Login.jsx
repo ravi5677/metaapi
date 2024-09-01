cf11f9f6f99c1bc83cdf50b12231305e924af14b
Ravi Prakash Awasthi <ravi.awasthi93@gmail.com>
2024-09-01 13:20:58 +0530

    changes

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -21,7 +21,6 @@ function checkLoginState() {
 window.fbAsyncInit = function () {
   FB.init({
     appId: "1035426781614046",
-    cookie: true, // Enable cookies to allow the server to access the session.
     xfbml: true, // Parse social plugins on this webpage.
     version: "v20.0", // Use this Graph API version for this call.
   });
