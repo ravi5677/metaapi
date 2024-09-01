7e9d01d5e1646d8f24acd040aa98d0e41d4bb9ff
Ravi Prakash Awasthi <ravi.awasthi93@gmail.com>
2024-09-01 17:04:40 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useSelector } from "react-redux";
 import "./App.css";
 import PageDropdown from "./components/PageDropdown";
 import Navbar from "./pages/Navbar";
-// import InsightCard from "./components/InsightCard";
+import InsightCard from "./components/InsightCard";
 
 function App() {
   const FB_INSIGHTS = useSelector((store) => store.FB_INSIGHTS);
@@ -11,9 +11,9 @@ function App() {
     <>
       <Navbar />
       <PageDropdown />
-      {/* {FB_INSIGHTS.map((item, ind) => {
-        return <InsightCard />;
-      })} */}
+      {FB_INSIGHTS.map((item, key) => {
+        return <InsightCard key={key} CardTitle={key} TitleValue={item} />;
+      })}
     </>
   );
 }
