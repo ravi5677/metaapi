866794c4a542e80da4522e77622f7007441a3ff7
Ravi Prakash Awasthi <ravi.awasthi93@gmail.com>
2024-09-01 19:47:58 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,10 @@ import InsightCard from "./components/InsightCard";
 
 function App() {
   const FB_INSIGHTS = useSelector((store) => store.FB_INSIGHTS);
-  const totalKeys = Object.keys(FB_INSIGHTS);
+  // const totalKeys = Object.keys(FB_INSIGHTS);
+  const totalKeys = Object.keys(obj).forEach(
+    (k) => obj[k] == null && delete obj[k]
+  );
   return (
     <>
       <Navbar />
