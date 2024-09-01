cb1ec02657ecaf0697db6a615b53c11767818d9e
Ravi Prakash Awasthi <ravi.awasthi93@gmail.com>
2024-09-01 16:40:46 +0530

    changes

---
+++ src/components/PageDropdown.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchFollowers } from "../Store/PageInsightSlice.";
+
+const PageDropdown = () => {
+  const dispatch = useDispatch();
+  const originalState = useSelector((store) => store.FB_GRAPH);
+  const [selPage, setSelPage] = useState({});
+  const pageList = originalState.pages;
+
+  const handleSubmitDrodown = () => {
+    if (selPage) {
+      const PageArr = pageList.filter((item) => {
+        return item.page_id == selPage;
+      });
+      const selectedPage = PageArr[0];
+      console.log("selectedPage", selectedPage);
+      dispatch(fetchFollowers(selectedPage));
+    } else {
+      alert("Please select a page to view insights");
+    }
+  };
+  return (
+    <div className="dropdownPageContainer">
+      <div className="dropdownPage">
+        <select
+          name="dropdown"
+          id="dropdown"
+          onChange={(e) => {
+            setSelPage(e.target.value);
+          }}
+        >
+          <option value="">Select A Page</option>
+          {pageList.map((item) => {
+            return (
+              <option value={item.page_id} key={item.page_id}>
+                {item.page_name}
+              </option>
+            );
+          })}
+        </select>
+        <button className="dropdownBtn" onClick={handleSubmitDrodown}>
+          Submit
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default PageDropdown;
