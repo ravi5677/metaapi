cb7dd41502647e517689a22aad51ffd32cda4e22
Ravi Prakash Awasthi <ravi.awasthi93@gmail.com>
2024-09-01 14:07:03 +0530

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import FbStore from "./Store/FbStore.jsx";
+import { Provider } from "react-redux";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <Provider store={FbStore}>
+      <App />
+    </Provider>
   </StrictMode>
 );
--- src/pages/FacebookLogin.jsx
+++ src/pages/FacebookLogin.jsx
@@ -1,45 +1,43 @@
 import React from "react";
 import { setSecretToken } from "../Store/FbSlice";
 import { useDispatch } from "react-redux";
-const dispatch = useDispatch();
+const FacebookLogin = () => {
+  const dispatch = useDispatch();
 
-const statusChangeCallback = (response) => {
-  console.log(response);
-  if (response.status === "connected") {
-    dispatch(
-      setSecretToken({
-        accessToken: response.accessToken,
-        userID: response.userID,
-      })
+  const statusChangeCallback = (response) => {
+    console.log(response);
+    if (response.status === "connected") {
+      dispatch(
+        setSecretToken({
+          accessToken: response.accessToken,
+          userID: response.userID,
+        })
+      );
+      console.log("Successfully logged in with Facebook");
+    } else if (response.status === "not_authorized") {
+      console.log("Logged into Facebook but not your app.");
+    } else {
+      console.log("Not logged into Facebook.");
+    }
+  };
+  const checkLoginState = () => {
+    FB.getLoginStatus(function (response) {
+      statusChangeCallback(response);
+    });
+  };
+
+  const handleButtonClick = () => {
+    FB.login(
+      function (response) {
+        checkLoginState(response);
+      },
+      { scope: "public_profile,email", config_id: "2335936246749582" }
     );
-    console.log("Successfully logged in with Facebook");
-  } else if (response.status === "not_authorized") {
-    console.log("Logged into Facebook but not your app.");
-  } else {
-    console.log("Not logged into Facebook.");
-  }
-};
+  };
 
-const checkLoginState = () => {
   FB.getLoginStatus(function (response) {
     statusChangeCallback(response);
   });
-};
-
-const handleButtonClick = () => {
-  FB.login(
-    function (response) {
-      checkLoginState(response);
-    },
-    { scope: "public_profile,email", config_id: "2335936246749582" }
-  );
-};
-
-FB.getLoginStatus(function (response) {
-  statusChangeCallback(response);
-});
-
-const FacebookLogin = () => {
   return (
     <div>
       <button onClick={handleButtonClick}>Login with Facebook</button>
