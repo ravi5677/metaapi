8dd302b2076ea75e94d65c4ea52c3ec69209032e
Ravi Prakash Awasthi <ravi.awasthi93@gmail.com>
2024-09-01 13:13:13 +0530

    changes

--- src/pages/FacebookLogin.jsx
+++ src/pages/FacebookLogin.jsx
@@ -1,4 +1,25 @@
 import React from "react";
+window.fbAsyncInit = function () {
+  FB.init({
+    appId: "1035426781614046",
+    xfbml: true,
+    version: "v20.0",
+  });
+
+  FB.AppEvents.logPageView();
+};
+
+(function (d, s, id) {
+  var js,
+    fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) {
+    return;
+  }
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "https://connect.facebook.net/en_US/sdk.js";
+  fjs.parentNode.insertBefore(js, fjs);
+})(document, "script", "facebook-jssdk");
 
 function statusChangeCallback(response) {
   console.log("statusChangeCallback");
---
+++ src/pages/Login.jsx
@@ -0,0 +1,56 @@
+function statusChangeCallback(response) {
+  console.log("statusChangeCallback");
+  console.log(response);
+  if (response.status === "connected") {
+    // Logged into your webpage and Facebook.
+    testAPI();
+  } else {
+    // Not logged into your webpage or we are unable to tell.
+    console.log("Please log " + "into this webpage.");
+  }
+}
+
+function checkLoginState() {
+  // Called when a person is finished with the Login Button.
+  FB.getLoginStatus(function (response) {
+    // See the onlogin handler
+    statusChangeCallback(response);
+  });
+}
+
+window.fbAsyncInit = function () {
+  FB.init({
+    appId: "1035426781614046",
+    cookie: true, // Enable cookies to allow the server to access the session.
+    xfbml: true, // Parse social plugins on this webpage.
+    version: "v20.0", // Use this Graph API version for this call.
+  });
+
+  FB.getLoginStatus(function (response) {
+    // Called after the JS SDK has been initialized.
+    statusChangeCallback(response); // Returns the login status.
+  });
+};
+
+function testAPI() {
+  // Testing Graph API after login.  See statusChangeCallback() for when this call is made.
+  console.log("Welcome!  Fetching your information.... ");
+  FB.api("/me", function (response) {
+    console.log("Successful login for: " + response.name);
+    document.getElementById("status").innerHTML =
+      "Thanks for logging in, " + response.name + "!";
+  });
+}
+
+const Login = () => {
+  return (
+    <div>
+      <fb:login-button
+        scope="public_profile,email"
+        onlogin="checkLoginState();"
+      ></fb:login-button>
+    </div>
+  );
+};
+
+export default Login;
