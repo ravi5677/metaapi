c4039120814f3cf11523cb62086df86806cf4814
Ravi Prakash Awasthi <ravi.awasthi93@gmail.com>
2024-09-01 17:53:54 +0530

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,11 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import FbStore from "./Store/FbStore.jsx";
 import { Provider } from "react-redux";
-import { CookiesProvider } from "react-cookie";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={FbStore}>
-      <CookiesProvider>
-        <App />
-      </CookiesProvider>
+      <App />
     </Provider>
   </StrictMode>
 );
