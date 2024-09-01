dd4b9811c532b60657d0c884a5a021164ede8d55
Ravi Prakash Awasthi <ravi.awasthi93@gmail.com>
2024-09-01 15:43:27 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,33 @@ import "./App.css";
 import Navbar from "./pages/Navbar";
 
 function App() {
+  const pageList = [];
   return (
     <>
       <Navbar />
+      <div className="dropdownPageContainer">
+        <div className="dropdownPage">
+          <select
+            name="dropdown"
+            id="dropdown"
+            onChange={(e) => {
+              setSelectedPageData(e.target.value);
+            }}
+          >
+            <option value="">Select A Page</option>
+            {pageList.map((item) => {
+              return (
+                <option value={item.id} key={item.id}>
+                  {item.name}
+                </option>
+              );
+            })}
+          </select>
+          <button className="dropdownBtn" onClick={handleSubmitDrodown}>
+            Submit
+          </button>
+        </div>
+      </div>
     </>
   );
 }
