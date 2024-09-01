106e35dbcca15efa92825d494ff83a9ef792c8c2
Ravi Prakash Awasthi <ravi.awasthi93@gmail.com>
2024-09-01 19:27:49 +0530

    changes

--- src/Store/PageInsightSlice..jsx
+++ src/Store/PageInsightSlice..jsx
@@ -44,10 +44,10 @@ export const fetchReactions = createAsyncThunk(
   async (args, thunkAPI) => {
     let Url = `${FB_BASE_URL}/${args.page_id}/insights/page_actions_post_reactions_total?access_token=${args.page_token}`;
     const apiData = await fetch(Url);
-    const { data } = await apiData.json();
-    const totalReactions = GET_TOTAL_INSIGHT_VALUE(data);
-    console.log("fetchReactions", data);
-    return totalReactions;
+    const apiDataResp = await apiData.json();
+    // const totalReactions = GET_TOTAL_INSIGHT_VALUE(data);
+    console.log("fetchReactions", apiDataResp);
+    return 0;
   }
 );
 
