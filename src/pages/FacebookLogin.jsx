3088fc9c3e38eaaa12ee61e1ebaf7535f71c3b16
Ravi Prakash Awasthi <ravi.awasthi93@gmail.com>
2024-09-01 14:03:13 +0530

    changes

--- src/pages/FacebookLogin.jsx
+++ src/pages/FacebookLogin.jsx
@@ -1,7 +1,39 @@
 import React from "react";
 import { setSecretToken } from "../Store/FbSlice";
 import { useDispatch } from "react-redux";
-import { handleButtonClick, statusChangeCallback } from "../Utils/FbSdk";
+const dispatch = useDispatch();
+
+const statusChangeCallback = (response) => {
+  console.log(response);
+  if (response.status === "connected") {
+    dispatch(
+      setSecretToken({
+        accessToken: response.accessToken,
+        userID: response.userID,
+      })
+    );
+    console.log("Successfully logged in with Facebook");
+  } else if (response.status === "not_authorized") {
+    console.log("Logged into Facebook but not your app.");
+  } else {
+    console.log("Not logged into Facebook.");
+  }
+};
+
+const checkLoginState = () => {
+  FB.getLoginStatus(function (response) {
+    statusChangeCallback(response);
+  });
+};
+
+const handleButtonClick = () => {
+  FB.login(
+    function (response) {
+      checkLoginState(response);
+    },
+    { scope: "public_profile,email", config_id: "2335936246749582" }
+  );
+};
 
 FB.getLoginStatus(function (response) {
   statusChangeCallback(response);
